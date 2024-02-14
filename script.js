b322b51e7740634ac3d79dababe7c9b4fd7f8b5c
sriyaratnam <sriyaepcao@gmail.com>
2024-02-14 20:33:05 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+var typed=new Typed(".typing",{
+    strings:[" ","Web Designer","Web Developer","Graphic Designer","YouTuber"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
+const nav=document.querySelector(".nav"),   
+    navList=nav.querySelectorAll("li"),
+    gotalNavList=navList.length;
+    for(let i=0;i<totalNavList;i++){
+        console.log(navList[i])
+    }
